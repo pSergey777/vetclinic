08cdda31f24092b041d9150b59939db9577a6bd8
pSergey777 <pSergey777@mail.ru>
2024-01-24 04:08:20 +0300

    добавил блок свайпера c js

--- script/carousel.js
+++ script/carousel.js
@@ -1,9 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+    slidesPerView: 3,
     spaceBetween: 30,
     freeMode: true,
+    // effect: 'coverflow',
+
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+
+});
+
