7dd8e9d84586f2a550f8507ec61f7d9e0e58321e
pSergey777 <pSergey777@mail.ru>
2024-01-26 01:27:48 +0300

    сделал адаптив для телефонов

--- script/accordion.js
+++ script/accordion.js
@@ -1,4 +1,6 @@
 const acc = document.getElementsByClassName("accordion");
+const groo  =document.querySelectorAll('.grooming-accordion_panel')
+console.log(groo);
 let i;
 
 for (i = 0; i < acc.length; i++) {
@@ -8,12 +10,20 @@ for (i = 0; i < acc.length; i++) {
         accArr.classList.toggle("accordion-arrow_open")
         const accImg = this.querySelector('img')
         const panel = this.nextElementSibling;
+        
         if (panel.style.display === "block") {
             panel.style.display = "none";
             accImg.src="/img/Arrow.svg"
         } else {
             panel.style.display = "block";
             accImg.src="/img/Arrow_down.svg"
+
+            if (i+1 === parseInt(select.value)) {
+                panel.style.display = "block";
+            } else {
+                panel.style.display = "none";
+            }
+            
         }
     });
 }
