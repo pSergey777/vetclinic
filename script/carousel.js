7e4548fae50b573763da7c570e829c76ca5eb1e4
pSergey777 <pSergey777@mail.ru>
2024-01-28 22:19:10 +0300

    исправил некоторые ошибки, доделал свайпер, поправил шрифт

--- script/carousel.js
+++ script/carousel.js
@@ -1,14 +1,45 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView:1,
   centeredSlides: true,
-    spaceBetween: 30,
-    // freeMode: true,
+  slidesPerView: 3,
+    spaceBetween: 50,
+    freeMode: true,
+    // slidesPerColumn: 1,
+    // loop: true, 
     // effect: 'coverflow',
 
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      // dynamicBullets: true, //включаем поддержку динамических буллетов
+      dynamicMainBullets: 2, //максимальное количество отображаемых буллетов
+
+      on: {
+        slideChangeTransitionStart: function () {
+          var activeSlide = this.slides[this.activeIndex];
+          var blockLeftPosition = document.getElementById('target-block').getBoundingClientRect().left;
+          
+          if (activeSlide.getBoundingClientRect().left < blockLeftPosition) {
+            activeSlide.style.backgroundColor = 'blue'; // изменяем цвет карточки, когда она проходит за блок слева
+          } else {
+            activeSlide.style.backgroundColor = 'initial'; // возвращаем исходный цвет карточки
+          }
+        },
+      },
     },
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
