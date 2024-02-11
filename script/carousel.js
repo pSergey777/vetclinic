2b5be507b61a128fd39f2c21d729f50ebc296095
pSergey777 <pSergey777@mail.ru>
2024-02-11 22:54:49 +0300

    переписал вторую половину, мелкие изменения, переписал js слайдера

--- script/carousel.js
+++ script/carousel.js
@@ -1,34 +1,61 @@
 var swiper = new Swiper(".mySwiper", {
   // centeredSlides: true,
-  initialSlide: 1, // устанавливаем второй слайд как начальный
+  initialSlide: 0, // устанавливаем второй слайд как начальный
   slidesPerView: 1,
   spaceBetween: 15,
   freeMode: true,
+
   // slidesPerColumn: 1,
-  // loop: true, 
+  loop: true,
   // effect: 'coverflow',
+
+
+  // slidesOffsetAfter: 50, // сдвигаем первый слайд влево на 50px
+  slidesOffsetBefore: -250, // сдвигаем первый слайд влево на 50px
 
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     // dynamicBullets: true, //включаем поддержку динамических буллетов
-    dynamicMainBullets: 2, //максимальное количество отображаемых буллетов
+    // dynamicMainBullets: 2, //максимальное количество отображаемых буллетов
+    initialSlide: 1, // устанавливаем вторую точку пагинации как начальную
+  },
 
-    on: {
-      slideChangeTransitionStart: function () {
-        var activeSlide = this.slides[this.activeIndex];
-        var blockLeftPosition = document.getElementById('target-block').getBoundingClientRect().left;
-
-        if (activeSlide.getBoundingClientRect().left < blockLeftPosition) {
-          activeSlide.style.backgroundColor = 'blue'; // изменяем цвет карточки, когда она проходит за блок слева
-        } else {
-          activeSlide.style.backgroundColor = 'initial'; // возвращаем исходный цвет карточки
-        }
-      },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      initialSlide: 0, // устанавливаем второй слайд как начальный
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40
     },
   },
 
+  // on: {
+  //   slideChangeTransitionStart: function () {
+  //     var activeSlide = this.slides[this.activeIndex];
+  //     var blockLeftPosition = document.getElementById('target-block').getBoundingClientRect().left;
+
+  //     if (activeSlide.getBoundingClientRect().left < blockLeftPosition) {
+  //       activeSlide.style.backgroundColor = 'blue'; // изменяем цвет карточки, когда она проходит за блок слева
+  //     } else {
+  //       activeSlide.style.backgroundColor = 'initial'; // возвращаем исходный цвет карточки
+  //     }
+  //   },
+  // },
 });
+
+
+
+
+
+// var swiper = new Swiper('.swiper-container', {
+//   // начальные настройки для слайдера
+
+
+// });
 
 
 
